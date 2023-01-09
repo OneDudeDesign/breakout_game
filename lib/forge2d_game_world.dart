408d157b06a75878c396a04e7444d06eab837406
Christopher Lucier <OneDudeDesign@gmail.com>
2023-01-09 06:42:29 -0800

    Arena added ball captured

--- lib/forge2d_game_world.dart
+++ lib/forge2d_game_world.dart
@@ -1,5 +1,6 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'components/ball.dart';
+import 'components/arena.dart';
 
 class Forge2dGameWorld extends Forge2DGame {
   @override
@@ -8,6 +9,8 @@ class Forge2dGameWorld extends Forge2DGame {
   }
 
   Future<void> _initializeGame() async {
+    final arena = Arena();
+    await add(arena);
     final ball = Ball(position: size / 2, radius: 1.0);
     await add(ball);
   }
